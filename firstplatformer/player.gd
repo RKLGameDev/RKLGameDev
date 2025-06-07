9b2460c961a3e908f32c7ffc39a353a566880186
drsestito93 <drsestito93@gmail.com>
2025-06-07 19:23:01 -0400

    Auto-sync: 2025-06-07 19:23:01

--- firstplatformer/player.gd
+++ firstplatformer/player.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 
-const speed = 1000
+const speed = 600
 var vel = Vector2.ZERO
-const friction = 200
+const friction = 3000
 var dir = Vector2(0,1)
-var acc = 20000
-var grav = 20000
+var acc = 6000
+const GRAVITY = 30000
+const jumpspeed = 1000
 
 func _physics_process(delta):
 	var input_vec = Vector2.ZERO
-		
+	var jump=0
 	input_vec.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vec.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vec = input_vec.normalized()
-	
+	jump = Input.get_action_strength("ui_select")
+	print(input_vec);print(jump)
 	if input_vec != Vector2.ZERO:# and health > 0:
 		vel = vel.move_toward(speed*input_vec, delta*acc)
 		dir = input_vec*(1/max(abs(input_vec.x),abs(input_vec.y)))
@@ -24,6 +26,7 @@ func _physics_process(delta):
 		vel = vel.move_toward(Vector2.ZERO, friction*delta)
 		#animationstate.travel("Idle")
 	velocity = vel	
-	velocity += Vector2(0, grav * delta)
+	if jump>0:
+		velocity.y = -jumpspeed
 # warning-ignore:return_value_discarded
 	move_and_slide()
