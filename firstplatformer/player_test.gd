66fea2c264da802b742a95abfe199b2a9be3fd46
drsestito93 <drsestito93@gmail.com>
2025-06-07 17:48:01 -0400

    Auto-sync: 2025-06-07 17:48:01

---
+++ firstplatformer/player_test.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const speed = 140
+var vel = Vector2.ZERO
+const friction = 20
+var dir = Vector2(0,1)
+var acc = 200
+
+func _physics_process(delta):
+	var input_vec = Vector2.ZERO
+		
+	input_vec.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vec.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vec = input_vec.normalized()
+	print(input_vec)
+	if input_vec != Vector2.ZERO:# and health > 0:
+		vel = vel.move_toward(speed*input_vec, delta*acc)
+		dir = input_vec*(1/max(abs(input_vec.x),abs(input_vec.y)))
+		#animationtree.set("parameters/Idle/blend_position",dir)
+		#animationtree.set("parameters/Run/blend_position",dir)
+		#animationstate.travel("Run")
+	else:
+		vel = vel.move_toward(Vector2.ZERO, friction*delta)
+		#animationstate.travel("Idle")
+	velocity = vel	
+# warning-ignore:return_value_discarded
+	move_and_slide()
