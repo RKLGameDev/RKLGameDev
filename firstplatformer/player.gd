812f386f8a6648187094a70aa49006a8d72a5f5d
Tyler B <typh15@yahoo.com>
2025-06-15 00:20:17 -0400

    Character Movement and Animation Polished

--- firstplatformer/player.gd
+++ firstplatformer/player.gd
@@ -1,21 +1,18 @@
 extends CharacterBody2D
 
-const GRAVITY = 980
-const SPEED = 1000.0
-const friction = 1000.0
-const ACCELERATION = 3000.0
-const JUMP_VELOCITY = -800.0  # Negative because Y-axis goes down in Godot
+const GRAVITY 		= 980
+const SPEED 		= 600.0
+const friction 		= 3000.0
+const ACCELERATION 	= 2000.0
+const JUMP_VELOCITY = -600.0  # Negative because Y-axis goes down in Godot
 
-var input_dir = 0
+var input_dir 		= 0
+var current_dir 	= 0
 
-@onready var animationplayer = $AnimationPlayer
-@onready var animationtree = $AnimationTree 
-@onready var animationstate = animationtree.get("parameters/playback")
-
+@onready var animatedsprite = $AnimatedSprite2D
 
 
 func _physics_process(delta):
-	
 	
 	# Get horizontal input
 	input_dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -26,31 +23,39 @@ func _physics_process(delta):
 	# Handle jumping - only if on ground
 	if Input.is_action_just_pressed("ui_select") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
 	
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	
-	
-	
 	# Handle horizontal movement
 	if input_dir != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_dir, ACCELERATION * delta)
+		#Treat Movement Differently when on ground vs in the air
+		if is_on_floor():
+			current_dir = input_dir
+			velocity.x = move_toward(velocity.x, SPEED * input_dir, ACCELERATION * delta)
+			# Animation code here
+			if input_dir > 0:
+				animatedsprite.play("RunRight")
+			if input_dir < 0:
+				animatedsprite.play("RunLeft")
+		else:
+			velocity.x = move_toward(velocity.x, SPEED * input_dir, ACCELERATION * delta/3)
 		# Animation code here
-		if input_dir > 0:
-			animationstate.travel("RunRight")
-		if input_dir < 0:
-			animationstate.travel("RunLeft")
+			if input_dir > 0:
+				animatedsprite.play("JumpRight")
+			if input_dir < 0:
+				animatedsprite.play("JumpLeft")
+	
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction * delta)
-		# Animation code here
-		animationstate.travel("Idle")
+		if is_on_floor():
+			current_dir = input_dir
+			velocity.x = move_toward(velocity.x, 0, friction * delta)
+			# Animation code here
+			animatedsprite.play("Idle")
 	
 	# Move the character - this handles collisions properly
 	move_and_slide()
 	
 	# Debug output
 	print("Input: ", input_dir, " On Floor: ", is_on_floor(), " Velocity: ", velocity)
-		
-		
