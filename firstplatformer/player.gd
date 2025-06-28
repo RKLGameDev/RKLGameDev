c867614cffd9bb833f628d4230e66e77b2df3a94
Tyler B <typh15@yahoo.com>
2025-06-28 15:53:22 -0400

    E

--- firstplatformer/player.gd
+++ firstplatformer/player.gd
@@ -24,6 +24,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
+	
+	
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
