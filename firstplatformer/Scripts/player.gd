ae05b18e2f44cefbc1defc0038b3742c9a656a03
Tyler B <typh15@yahoo.com>
2025-07-12 03:28:47 -0400

    Title Screen and Goofy mechanics
    Can grow and shrink with "n"/"m". Also doublejump with animation

--- firstplatformer/Scripts/player.gd
+++ firstplatformer/Scripts/player.gd
@@ -9,24 +9,50 @@ extends CharacterBody2D
 #  A.1 -------------- Movement -------------- #
 	
 #    A.1.1 ------ Movement Constants ------ #
-const GRAVITY 		= 980
-const run_speed		= 500.0
-const friction 		= 2800.0
-const acceleration 	= 1500.0
-const jump_velocity = -1400.0 
-const air_cont_red  = 4
-const sprint_speed  = 850.0
-const sprint_acc 	= 1200.0
-const down_jump_red = 100.0
-const up_jump_incr  = -100.0
+			
+#      A.1.1.1 ------ On Ground ------ #
+const base_run_speed		= 500.0
+const base_friction 		= 2800.0
+const base_acceleration 	= 1700.0
+const base_sprint_speed     = 850.0
+const base_sprint_acc 	    = 1500.0
+
+#      A.1.1.2 -------- In Air ------- #
+const gravity 		        = 980
+const base_jump_velocity    = -700.0 
+const base_air_cont_red     = 4  # Determines how much control the character has in the air
+const base_down_jump_red    = 100.0
+const base_up_jump_incr     = -100.0
+
+
 
 #    A.1.2 ------ Movement Variables ------ #
-var jump = false
+
+#      A.1.1.1 ------ On Ground ------ #
+
+var run_speed		= base_run_speed	
+var friction 		= base_friction 	
+var acceleration 	= base_acceleration
+var sprint_speed    = base_sprint_speed
+var sprint_acc 	    = base_sprint_acc 	
+
+#      A.1.1.2 -------- In Air ------- #
+var jump_velocity   = base_jump_velocity
+var air_cont_red    = base_air_cont_red 
+var down_jump_red   = base_down_jump_red
+var up_jump_incr    = base_up_jump_incr 
+
+var jump            = false
+var doublejump_active = true
+var doublejump      = true
 var input_dir 		= 0
 var current_dir 	= 0
-var speed           = run_speed
+var speed           = 0
 
-	
+
+
+
+
 #  A.2 --------------- Falling -------------- #
 	
 #    A.2.1 ------ Falling Constants ------ #
@@ -43,7 +69,9 @@ var fall_damageable = 0
 	
 #    A.3.1 ------- Loaded Variables ------ #
 @onready var animatedsprite = $AnimatedSprite2D
+@onready var doublejumpanim = $DoubleJumpEffect
 @onready var eye_animation  = $Eye/AnimatedSprite2D
+@onready var camera         = $Camera2D
 
 
 
@@ -53,8 +81,13 @@ var fall_damageable = 0
 #    A.4.1 ------- Others Constants ------- #
 
 #    A.4.2 ------- Others Variables ------- #
-var health = 4
-var alive  = true
+var health        = 4
+var alive         = true
+var scale_tracker = 0
+var scale_factor  = 4.0/3.0
+
+
+
 
 
 # ------------------------------------------------------------------------------ #
@@ -67,22 +100,22 @@ func took_damage(current_health):
 	else:
 		if current_health == 4:
 			eye_animation.play("Eye1")
-			#eye_animation.scale = Vector2(eye_animation.scale[0]*1.25, eye_animation.scale[1]*1.25)
 		if current_health == 3:
 			eye_animation.play("Eye2")
-			#eye_animation.scale = Vector2(eye_animation.scale[0]*1.5, eye_animation.scale[1]*1.5)
 		if current_health == 2:
 			eye_animation.play("Eye3")
-			#eye_animation.scale = Vector2(eye_animation.scale[0]*2, eye_animation.scale[1]*2)
 		if current_health == 1:
 			eye_animation.play("Eye4")
-			#eye_animation.scale = Vector2(eye_animation.scale[0]*4, eye_animation.scale[1]*4 )
 		return(current_health - 1)
+
+
+
 
 
 # ------------------------------------------------------------------------------ #
 # ----------------------------- C : PHYSICS PROCESS ---------------------------- #
 # ------------------------------------------------------------------------------ #
+
 func _physics_process(delta):
 	
 #  C.1 -------------- Movement -------------- #
@@ -94,7 +127,8 @@ func _physics_process(delta):
 		input_dir = input_dir/abs(input_dir)
 		
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta  # Apply gravity
+		#velocity.y += gravity * delta  # Apply gravity
+		velocity.y = move_toward(velocity.y, 9000000000000000000, gravity * delta)
 	
 	if input_dir != 0: # Handle horizontal movement
 		if is_on_floor(): #Treat Movement Differently when on ground vs in the air
@@ -131,6 +165,7 @@ func _physics_process(delta):
 #    C.1.2 ---- Movement Features ----- #
 
 #      C.1.2.1 ------ Sprinting ------- #
+
 	if is_on_floor():
 		if Input.get_action_strength("sprint"):
 			speed = sprint_speed
@@ -138,6 +173,7 @@ func _physics_process(delta):
 			speed = run_speed
 			
 #      C.1.2.2 ------- Jumping ------- #
+
 	if Input.is_action_just_pressed("PermaJumpOn"):
 		jump = true
 	if Input.is_action_just_pressed("PermaJumpOff"):
@@ -145,21 +181,26 @@ func _physics_process(delta):
 		
 	var jumpnow = (Input.is_action_just_pressed("jump") or jump)
 	
-	if jumpnow and is_on_floor(): # Handle jumping - only if on ground
+	if jumpnow and (is_on_floor() or (doublejump_active and doublejump)): # Handle jumping - only if on ground
+		if not is_on_floor():
+			doublejumpanim.play("DoubleJump")
+			
+		doublejump = false
 		if Input.get_action_strength("move_down"):
 			velocity.y = jump_velocity + down_jump_red
 		elif Input.get_action_strength("move_up"):
 			velocity.y = jump_velocity + up_jump_incr
 		else:
 			velocity.y = jump_velocity
-	
+	if is_on_floor() and doublejump_active:
+		doublejump = true
 	
 	
 	
 #  C.2 --------- Health and Damage ---------- #
 	
 #    C.2.1 -------- Fall Damage ------- #
-	
+
 	if self.velocity.y > fall_damage_vel:
 		fall_damageable = 1
 		
@@ -170,8 +211,34 @@ func _physics_process(delta):
 	if self.velocity.y < fall_damage_vel:
 		last_ok_height  = self.position.y
 		fall_damageable = 0
-			
-			
-			
-	print(health)
-			
+	
+	
+	
+#  C.3 ---------- Other Mechanics ----------- #
+	
+#    C.3.1 ------ Shink and Grow ------ #a
+
+	if Input.is_action_just_pressed("Shrink") and scale_tracker > -4:
+		scale = (1.0/scale_factor) * scale
+		camera.zoom = scale_factor * camera.zoom
+		scale_tracker -= 1
+		print("Scale: ", scale_tracker)	
+		
+	if Input.is_action_just_pressed("Grow") and scale_tracker < 4:
+		scale = scale_factor * scale
+		camera.zoom = (1.0/scale_factor) * camera.zoom
+		scale_tracker += 1
+		print("Scale: ", scale_tracker)	
+
+	sprint_speed    = (scale_factor ** (scale_tracker/2)) * base_sprint_speed 
+	run_speed		= (scale_factor ** (scale_tracker/2)) * base_run_speed
+	air_cont_red    = max(2, base_air_cont_red + (0.5 * scale_tracker))
+	print("air_cont_red: ", air_cont_red)
+
+	acceleration 	= (scale_factor ** scale_tracker)     * base_acceleration 
+	sprint_acc 	    = (scale_factor ** scale_tracker)     * base_sprint_acc 	 
+	friction 		= (scale_factor ** scale_tracker)     * base_friction 	
+
+	jump_velocity   = (scale_factor ** (scale_tracker/2)) * base_jump_velocity
+	down_jump_red   = (scale_factor ** (scale_tracker/2)) * base_down_jump_red
+	up_jump_incr    = (scale_factor ** (scale_tracker/2)) * base_up_jump_incr 
