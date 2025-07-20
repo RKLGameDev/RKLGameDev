741f6a34b359c129bfc8213f3403f7e14c2351fb
Tyler B <typh15@yahoo.com>
2025-07-20 16:36:41 -0400

    up

--- firstplatformer/Scenes/Object Scenes/door.gd
+++ firstplatformer/Scenes/Object Scenes/door.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
+@onready var doorsprite = $AnimatedSprite2D
+
 
 @warning_ignore("unused_parameter")
-func _on_pot_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		if amount_of_coffee > 0:
-			amount_of_coffee = amount_of_coffee - 1
-
-
 func _on_open_door_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if doorsprite.frame == 8:
+			doorsprite.play_backwards()
+
+
+@warning_ignore("unused_parameter")
+func _on_closed_door_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if doorsprite.frame == 0:
+			doorsprite.play()
